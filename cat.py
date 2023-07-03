d87f058822c73b4ba60f51b92e140ff3732047d6
sirilaktem <sirilaktem@outlook.com>
2023-07-02 18:19:14 -0700

    Meow n times based on user input using for loop

--- cat.py
+++ cat.py
@@ -10,4 +10,14 @@ for i in range(3):
     print("Meow")
 '''
 
+'''
 print("Meow\n" * 3, end="")
+'''
+
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("Meow")

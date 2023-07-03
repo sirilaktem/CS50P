9dabdf20397e2fd3d16f2f312f1c6bc4866da877
sirilaktem <sirilaktem@outlook.com>
2023-07-02 18:38:55 -0700

    Meow n times based on user input using multiple functions

--- cat.py
+++ cat.py
@@ -14,6 +14,7 @@ for i in range(3):
 print("Meow\n" * 3, end="")
 '''
 
+'''
 while True:
     n = int(input("What's n? "))
     if n > 0:
@@ -21,3 +22,24 @@ while True:
 
 for _ in range(n):
     print("Meow")
+'''
+
+
+def main():
+    number = get_number()
+    meow(number)
+
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            return n
+
+
+def meow(n):
+    for _ in range(n):
+        print("Meow")
+
+
+main()

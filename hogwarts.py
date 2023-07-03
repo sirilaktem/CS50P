4eac26b2566994f66bff7dba8726a162eae093fb
sirilaktem <sirilaktem@outlook.com>
2023-07-03 08:10:11 -0700

    Loop through a list of dictionaries using for in loop

--- hogwarts.py
+++ hogwarts.py
@@ -12,6 +12,7 @@ for i in range(len(students)):
     print(i + 1, students[i])
 '''
 
+'''
 students = {"Hermione": "Gryffindor",
             "Harry": "Gryffindor",
             "Ron": "Gryffindor",
@@ -20,3 +21,38 @@ students = {"Hermione": "Gryffindor",
 
 for student in students:
     print(student, students[student], sep=", ")
+'''
+
+'''
+    name        house       patronus
+0   Hermione    Gryffindor  Otter
+1   Harry       Gryffindor  Stag
+2   Ron         Gryffindor  Jack Russell terrier
+3   Draco       Slytherin
+'''
+
+students = [
+    {
+        "name": "Hermione",
+        "house": "Gryffindor",
+        "patronus": "Otter"
+    },
+    {
+        "name": "Harry",
+        "house": "Gryffindor",
+        "patronus": "Stag"
+    },
+    {
+        "name": "Ron",
+        "house": "Gryffindor",
+        "patronus": "Jack Russell terrier"
+    },
+    {
+        "name": "Draco",
+        "house": "Slytherin",
+        "patronus": None
+    }
+]
+
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")

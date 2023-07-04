ddbb47a04c0144cb28e809e8aca4828402d1c25f
sirilaktem <sirilaktem@outlook.com>
2023-07-03 22:02:45 -0700

    Sample of using try, except, and else

--- number.py
+++ number.py
@@ -1,5 +1,14 @@
+'''
 try:
     x = int(input("What's x? "))
     print(f"x is {x}")
 except ValueError:
     print("x is not an integer")
+'''
+
+try:
+    x = int(input("What's x? "))
+except ValueError:
+    print("x is not an integer")
+else:
+    print(f"x is {x}")

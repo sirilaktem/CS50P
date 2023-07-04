8845fad4247f34004814ff3181e52b9e60699e3f
sirilaktem <sirilaktem@outlook.com>
2023-07-03 21:27:57 -0700

    Print 2 dimensional wall

--- mario.py
+++ mario.py
@@ -1,9 +1,16 @@
 def main():
-    print_row(4)
+    print_square(3)
 
 
-def print_row(width):
-    print("?" * width)
+def print_square(size):
+    # For each row in square
+    for i in range(size):
+        # For each brick in row
+        for j in range(size):
+            # Print brick
+            print("#", end="")
+        # End of row, start the new line
+        print()
 
 
 main()

2fd35a993746bd58ab5190d690e469323f12141d
sirilaktem <sirilaktem@outlook.com>
2023-07-05 22:47:45 -0700

    Change to functions and handle the except using pass

--- number.py
+++ number.py
@@ -6,12 +6,18 @@ except ValueError:
     print("x is not an integer")
 '''
 
-while True:
-    try:
-        x = int(input("What's x? "))
-    except ValueError:
-        print("x is not an integer")
-    else:
-        break
 
-print(f"x is {x}")
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            pass
+
+
+main()

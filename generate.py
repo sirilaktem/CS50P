1bb2acdea771cb66b4db865aede48a83090ab1fc
sirilaktem <sirilaktem@outlook.com>
2023-07-06 18:34:47 -0700

    Add all samples in the script

--- generate.py
+++ generate.py
@@ -1,5 +1,15 @@
 import random
 
+'''
+coin = random.choice(["heads", "tails"])
+print(coin)
+'''
+
+'''
+number = random.randint(1, 10)
+print(number)
+'''
+
 cards = ["jack", "queen", "king"]
 random.shuffle(cards)
 for card in cards:

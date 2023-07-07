71837271e866ae9f35ab4fbf381f01a117752e8b
sirilaktem <sirilaktem@outlook.com>
2023-07-06 21:11:12 -0700

    Eliminate elif condition and loop through multi arguments by slicing the first arg

--- name.py
+++ name.py
@@ -1,8 +1,16 @@
 import sys
 
+'''
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
 elif len(sys.argv) > 2:
     sys.exit("Too many arguments")
 
 print("hello, my name is", sys.argv[1])
+'''
+
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+
+for arg in sys.argv[1:]:
+    print("hello, myname is", arg)

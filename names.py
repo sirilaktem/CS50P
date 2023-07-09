f05175ec5c16ba29f526b8b0047279d77eb01c20
sirilaktem <sirilaktem@outlook.com>
2023-07-09 13:57:33 -0700

    Read existing file using read mode

--- names.py
+++ names.py
@@ -1,6 +1,14 @@
+''' Write a file
 name = input("What's your name? ")
 
 # "w" for write | "a" for append
-file = open("names.txt", "a")
-file.write(f"{name}\n")
-file.close()
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
+'''
+
+# Read exsiting file using "r" for read mode
+with open("names.txt", "r") as file:
+    lines = file.readlines()
+
+for line in lines:
+    print("hello,", line)

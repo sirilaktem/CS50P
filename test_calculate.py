712330e898542a7effb11895ebc993540d9cd056
sirilaktem <sirilaktem@outlook.com>
2023-07-08 23:43:58 -0700

    Improve the code by separating to multiple test cases

--- test_calculate.py
+++ test_calculate.py
@@ -1,12 +1,28 @@
 from calculate import square
 
-
+'''
 def test_square():
     assert square(2) == 4
     assert square(3) == 9
     assert square(-2) == 4
     assert square(-3) == 9
     assert square(0) == 0
+'''
+
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+
+def test_zero():
+    assert square(0) == 0
+
 
 # def main():
 #    test_square()

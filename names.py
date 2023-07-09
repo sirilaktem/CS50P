976cdd2f9b3f1741e1e5ba32a7ebfecf69d02633
sirilaktem <sirilaktem@outlook.com>
2023-07-09 14:09:06 -0700

    Sort the list from Z-A

--- names.py
+++ names.py
@@ -19,5 +19,5 @@ with open("names.txt") as file:
     for line in file:
         names.append(line.strip())
 
-for name in sorted(names):
+for name in sorted(names, reverse=True):
     print(f"hello, {name}")

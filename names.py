33ea392c6fc4c599c3b47b9c71e0186c9a9e2279
sirilaktem <sirilaktem@outlook.com>
2023-07-09 14:06:21 -0700

    Read all lines from the file then store in a list and sort then display them

--- names.py
+++ names.py
@@ -7,6 +7,17 @@ with open("names.txt", "a") as file:
 '''
 
 # Read exsiting file using "r" for read mode
+'''
 with open("names.txt", "r") as file:
     for line in file:
         print("hello,", line.rstrip())
+'''
+
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.strip())
+
+for name in sorted(names):
+    print(f"hello, {name}")

19a72d14c7235e412958f0533cc9ac8ef9ace15a
sirilaktem <sirilaktem@outlook.com>
2023-07-09 14:00:10 -0700

    Improve how to iterate when reading the file

--- names.py
+++ names.py
@@ -8,7 +8,5 @@ with open("names.txt", "a") as file:
 
 # Read exsiting file using "r" for read mode
 with open("names.txt", "r") as file:
-    lines = file.readlines()
-
-for line in lines:
-    print("hello,", line.rstrip())
+    for line in file:
+        print("hello,", line.rstrip())

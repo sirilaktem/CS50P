7a3ef601e0ff9e1696f97a0069b67ee2094f023a
sirilaktem <sirilaktem@outlook.com>
2023-07-09 00:03:27 -0700

    Break down the test cases

--- test_hellofn.py
+++ test_hellofn.py
@@ -1,5 +1,9 @@
 from hellofn import hello
 
 
-def test_hello():
-    hello("David") == "hello, David"
+def test_default():
+    assert hello() == "hello, world"
+
+
+def test_argument():
+    assert hello("David") == "hello, David"

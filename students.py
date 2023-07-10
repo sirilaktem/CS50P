75ae29a47b11e3f3c8f1c5426b844ee557963287
sirilaktem <sirilaktem@outlook.com>
2023-07-09 17:34:10 -0700

    Try to sort data by house descending

--- students.py
+++ students.py
@@ -15,8 +15,8 @@ with open("students.csv") as file:
 
 
 def get_name(student):
-    return student["name"]
+    return student["house"]
 
 
-for student in sorted(students, key=get_name):
+for student in sorted(students, key=get_name, reverse=True):
     print(f"{student['name']} is in {student['house']}")

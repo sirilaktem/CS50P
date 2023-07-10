628c45e49edae5d2fac779afb49d62b37f6f0acb
sirilaktem <sirilaktem@outlook.com>
2023-07-09 17:33:09 -0700

    Use dictionries to store the data and create a function to sort data by student name

--- students.py
+++ students.py
@@ -1,4 +1,22 @@
+'''
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
         print(f"{name} is in {house}")
+'''
+
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.strip().split(",")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+
+def get_name(student):
+    return student["name"]
+
+
+for student in sorted(students, key=get_name):
+    print(f"{student['name']} is in {student['house']}")

21a16686004918a9dd74440855ab1ae683b7692d
sirilaktem <sirilaktem@outlook.com>
2023-07-09 18:31:15 -0700

    Use lambda instead of function

--- students.py
+++ students.py
@@ -13,10 +13,5 @@ with open("students.csv") as file:
         student = {"name": name, "house": house}
         students.append(student)
 
-
-def get_name(student):
-    return student["house"]
-
-
-for student in sorted(students, key=get_name, reverse=True):
+for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")

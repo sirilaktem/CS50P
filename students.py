32e0555960aede4bb7a162e18a397a92318398d0
sirilaktem <sirilaktem@outlook.com>
2023-07-09 18:44:38 -0700

    Utilize csv and reader to read csv file instead of writing split data logic

--- students.py
+++ students.py
@@ -5,13 +5,27 @@ with open("students.csv") as file:
         print(f"{name} is in {house}")
 '''
 
+'''
 students = []
 
 with open("students.csv") as file:
     for line in file:
-        name, house = line.strip().split(",")
-        student = {"name": name, "house": house}
+        name, home = line.strip().split(",")
+        student = {"name": name, "home": home}
         students.append(student)
 
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")
+    print(f"{student['name']} is from {student['home']}")
+'''
+
+
+import csv
+students = []
+
+with open("students.csv") as file:
+    reader = csv.reader(file)
+    for name, home in reader:
+        students.append({"name": name, "home": home})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")

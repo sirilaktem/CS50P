dae1fc351cc76208cf05ed5ca4437567c333d895
sirilaktem <sirilaktem@outlook.com>
2023-07-14 21:48:28 -0700

    Restrict by allowing only [a-zA-Z0-9_] before and after @ sign

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email? ").strip()
 
-if re.search(r"^[^@]+@[^@]+\.edu$", email):
+if re.search(r"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.edu$", email):
     print("Valid")
 else:
     print("Invalid")
